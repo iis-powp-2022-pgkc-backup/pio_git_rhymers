e8c1f9d9cd746fa860246909cfe736102f2eb309
backs <26674182+Backss4@users.noreply.github.com>
2021-12-06 19:29:00 +0100

    Zadanie 1.3.2: -poprawa formatowania plików -sprawdzenie działania kombinacji klawiszy alt + ← oraz alt + →

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,16 +2,21 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends defaultCountingOutRhymer {
 
-int totalRejected = 0;
+    int totalRejected = 0;
 
-	public int reportRejected() {
-		return totalRejected;
-	}
+    public int reportRejected() {
+        return totalRejected;
+    }
 
-	public void countIn(int in) {
-	if (!callCheck() && in > peekaboo())
-			totalRejected++;
-			else
-				super.countIn(in);
-	}
+    public void countIn(int in) {
+        if (!callCheck() && in > peekaboo())
+            totalRejected++;
+        else
+            super.countIn(in);
+    }
 }
+
+// alt + ← - zmiana pliku w lewo od bieżącego pliku
+// alt + → - zmiana pliku w lewo od bieżącego pliku
+// IDE wybiera plik z aktualnych otwartych i widocznych na pasku.
+
