ca6d2656c4a62b705043fb7be37fb3f6a0393f40
S-T-A-L-K-R <name.generate@gmail.com>
2021-12-06 11:01:14 +0100

    Revert "Merge commit '34cef32d2e78e0356d6d79406cacd51b517c8cae'"
    This reverts commit 35c670b229ccebbf189d901011b8aefec9da91c8.

--- src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
@@ -1,32 +1,32 @@
 package edu.kis.vh.nursery.factory;
 
-import edu.kis.vh.nursery.DefaultCountingOutRhymer;
+import edu.kis.vh.nursery.defaultCountingOutRhymer;
 import edu.kis.vh.nursery.FIFORhymer;
 import edu.kis.vh.nursery.HanoiRhymer;
-import edu.kis.vh.nursery.factory.RhymersFactory;
+import edu.kis.vh.nursery.factory.Rhymersfactory;
 
-public class DefaultRhymersFactory implements RhymersFactory 
+public class DefaultRhymersFactory implements Rhymersfactory 
 {
 	@Override
-	public DefaultCountingOutRhymer GetStandardRhymer() 
+	public defaultCountingOutRhymer GetStandardRhymer() 
 	{
-		return new DefaultCountingOutRhymer();
+		return new defaultCountingOutRhymer();
 	}
 
 	@Override
-	public DefaultCountingOutRhymer GetFalseRhymer() 
+	public defaultCountingOutRhymer GetFalseRhymer() 
 	{
-		return new DefaultCountingOutRhymer();
+		return new defaultCountingOutRhymer();
 	}
 
 	@Override
-	public DefaultCountingOutRhymer GetFIFORhymer() 
+	public defaultCountingOutRhymer GetFIFORhymer() 
 	{
 		return new FIFORhymer();
 	}
 
 	@Override
-	public DefaultCountingOutRhymer GetHanoiRhymer() 
+	public defaultCountingOutRhymer GetHanoiRhymer() 
 	{
 		return new HanoiRhymer();
 	}
--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -1,16 +1,16 @@
 package edu.kis.vh.nursery;
 
-import edu.kis.vh.nursery.DefaultCountingOutRhymer;
+import edu.kis.vh.nursery.defaultCountingOutRhymer;
 import edu.kis.vh.nursery.HanoiRhymer;
 import edu.kis.vh.nursery.factory.DefaultRhymersFactory;
-import edu.kis.vh.nursery.factory.RhymersFactory;
+import edu.kis.vh.nursery.factory.Rhymersfactory;
 
 class RhymersDemo {
 
 	public static void main(String[] args) {
-		RhymersFactory factory = new DefaultRhymersFactory();
+		Rhymersfactory factory = new DefaultRhymersFactory();
 		
-		DefaultCountingOutRhymer[] rhymers = { factory.GetStandardRhymer(), factory.GetFalseRhymer(),
+		defaultCountingOutRhymer[] rhymers = { factory.GetStandardRhymer(), factory.GetFalseRhymer(),
 				factory.GetFIFORhymer(), factory.GetHanoiRhymer()};
 		
 		for (int i = 1; i < 15; i++)
