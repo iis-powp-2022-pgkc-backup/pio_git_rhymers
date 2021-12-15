1d7a71f31f5f0c31b2dd70a89b2257eb2ebf34ec
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2021-12-15 16:12:58 +0100

    made intellij autoformat (1.3)

--- src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
@@ -6,24 +6,24 @@ import edu.kis.vh.nursery.HanoiRhymer;
 
 public class DefaultRhymersFactory implements Rhymersfactory {
 
-	@Override
-	public DefaultCountingOutRhymer GetStandardRhymer() {
-		return new DefaultCountingOutRhymer();
-	}
+    @Override
+    public DefaultCountingOutRhymer GetStandardRhymer() {
+        return new DefaultCountingOutRhymer();
+    }
 
-	@Override
-	public DefaultCountingOutRhymer GetFalseRhymer() {
-		return new DefaultCountingOutRhymer();
-	}
+    @Override
+    public DefaultCountingOutRhymer GetFalseRhymer() {
+        return new DefaultCountingOutRhymer();
+    }
 
-	@Override
-	public DefaultCountingOutRhymer GetFIFORhymer() {
-		return new FIFORhymer();
-	}
+    @Override
+    public DefaultCountingOutRhymer GetFIFORhymer() {
+        return new FIFORhymer();
+    }
 
-	@Override
-	public DefaultCountingOutRhymer GetHanoiRhymer() {
-		return new HanoiRhymer();
-	}
+    @Override
+    public DefaultCountingOutRhymer GetHanoiRhymer() {
+        return new HanoiRhymer();
+    }
 
 }
