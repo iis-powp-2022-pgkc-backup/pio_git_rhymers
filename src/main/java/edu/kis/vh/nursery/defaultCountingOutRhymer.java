06a3e3a8d252dd09fc1792dc7bb813657356c23a
Piotr Bednarek <specjunior@gmail.com>
2021-12-20 11:39:54 +0100

    format

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -2,14 +2,8 @@ package edu.kis.vh.nursery;
 
 public class defaultCountingOutRhymer {
 
-	/**
-	 *
-	 */
 	private static final int _12 = 12;
 
-	/**
-	 *
-	 */
 	private static final int MINUS_ONE = -1;
 
 	private int[] NUMBERS = new int[_12];
@@ -21,24 +15,24 @@ public class defaultCountingOutRhymer {
 			NUMBERS[++total] = in;
 	}
 
-		public boolean callCheck() {
-			return total == MINUS_ONE;
-		}
-		
-			public boolean isFull() {
-				return total == 11;
-			}
-		
-				protected int peekaboo() {
-					if (callCheck())
-						return MINUS_ONE;
-					return NUMBERS[total];
-				}
-			
-					public int countOut() {
-						if (callCheck())
-							return MINUS_ONE;
-						return NUMBERS[total--];
-					}
+	public boolean callCheck() {
+		return total == MINUS_ONE;
+	}
+
+	public boolean isFull() {
+		return total == 11;
+	}
+
+	protected int peekaboo() {
+		if (callCheck())
+			return MINUS_ONE;
+		return NUMBERS[total];
+	}
+
+	public int countOut() {
+		if (callCheck())
+			return MINUS_ONE;
+		return NUMBERS[total--];
+	}
 
 }
