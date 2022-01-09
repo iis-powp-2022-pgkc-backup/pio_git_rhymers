96c0cf08f2e816a6b30f50569c62d6d3b409efc1
Vermilion <misterpig231@gmail.com>
2022-01-09 19:43:58 +0100

    done tasks from stałe i adnotacje

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,32 +2,35 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-	private int[] numbers = new int[12]; // final albo malymi
+	private final int[] numbers = new int[size]; // final albo malymi
 
 	public int total = -1;
+
+	final static int size = 12;
+	final static int empty = -1;
 
 	public void countIn(int in) {
 		if (!isFull())
 			numbers[++total] = in;
 	}
-
 	public boolean callCheck() {
-		return total == -1;
+		return total == empty;
 	}
 
+
 	public boolean isFull() {
-		return total == 11;
+		return total == size-empty;
 	}
 
 	protected int peekaboo() {
 		if (callCheck())
-			return -1;
+			return empty;
 		return numbers[total];
 	}
 
 	public int countOut() {
 		if (callCheck())
-			return -1;
+			return empty;
 		return numbers[total--];
 	}
 
