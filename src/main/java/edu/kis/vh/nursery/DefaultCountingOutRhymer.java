06890454b85d03891dabd75a12f9a2c3ce2d4d0b
Konrad Basta <228685@edu.p.lodz.pl>
2022-02-02 23:37:41 +0100

    Wygenerowanie stałych

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,12 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-	private int[] numbers = new int[12];
+	public static final int INT = -1;
+	public static final int INT1 = 11;
+	public static final int INT2 = 12;
+	private int[] numbers = new int[INT2];
 
-	public int total = -1;
+	public int total = INT;
 
 	public void countIn(int in) {
 		if (!isFull())
@@ -12,22 +15,22 @@ public class DefaultCountingOutRhymer {
 	}
 
 	public boolean callCheck() {
-		return total == -1;
+		return total == INT;
 	}
 
 	public boolean isFull() {
-		return total == 11;
+		return total == INT1;
 	}
 
 	protected int peekaboo() {
 		if (callCheck())
-			return -1;
+			return INT;
 		return numbers[total];
 	}
 
 	public int countOut() {
 		if (callCheck())
-			return -1;
+			return INT;
 		return numbers[total--];
 	}
 
--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -2,6 +2,7 @@ package edu.kis.vh.nursery.list;
 
 public class IntLinkedList {
 
+	public static final int INT = -1;
 	Node last;
 	int i;
 
@@ -31,7 +32,7 @@ public class IntLinkedList {
 
 	public int pop() {
 		if (isEmpty())
-			return -1;
+			return INT;
 		int ret = last.value;
 		last = last.prev;
 		return ret;
