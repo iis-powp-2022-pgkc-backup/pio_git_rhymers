f3157ef88f3d9fdc6d5e7075daa4a96b3b57e184
Piotr Bednarek <specjunior@gmail.com>
2022-02-14 10:50:19 +0100

    fixed  Wrong name (it would be describe usages of EMPTY constant)

--- src/main/java/edu/kis/vh/nursery/list/IntArrayStack.java
+++ src/main/java/edu/kis/vh/nursery/list/IntArrayStack.java
@@ -1,18 +1,16 @@
 package edu.kis.vh.nursery.list;
 
 public class IntArrayStack implements IntArrayStackInterface {
-    private static final int _12 = 12;
+	private static final int MAX_STACK = 11;
 
 
-    private static final int DEFAULT_VALUE = 0;
-    /**
-     *
-     */
+    private static final int IF_EMPTY = 0;
+
     static final int EMPTY = 0;
 
-    private final int[] NUMBERS = new int[_12];
+    private final int[] NUMBERS = new int[MAX_STACK+1];
 
-    public int total = DEFAULT_VALUE;
+    public int total = IF_EMPTY;
 
     public void countIn(int in) {
         if (!isFull())
@@ -25,7 +23,7 @@ public class IntArrayStack implements IntArrayStackInterface {
 
     @Override
     public boolean isFull() {
-        return total == 11;
+        return total == MAX_STACK;
     }
 
     public int peekaboo() {
